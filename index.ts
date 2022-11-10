9874df960d7c07aa615b378796ab6193db74061b
diegoavellanedatorres <diego.avellaneda@somoglobal.com>
2022-11-09 19:05:03 -0500

    feat: add remote authentication to app

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import express, { Express, Request, Response } from 'express'
 import dotenv from 'dotenv'
 import qrcode from 'qrcode-terminal'
-import { Client, LocalAuth } from 'whatsapp-web.js'
+import { Client, LocalAuth, RemoteAuth } from 'whatsapp-web.js'
+import { MongoStore } from 'wwebjs-mongo'
+import mongoose from 'mongoose'
 
 dotenv.config()
 
@@ -10,6 +12,7 @@ let client: Client
 const port = process.env.PORT
 const MULTI_DEVICE = process.env.MULTI_DEVICE || 'true'
 const parentNumber = process.env.PARENTNUMBER
+const databaseURL = process.env.MONGO_DB || 'unknowdn'
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server')
@@ -28,37 +31,48 @@ const listenMessage = () => {
     return
   })
 }
+const dataBaseConnection = async () => {
+  const UserSchema = new mongoose.Schema({ name: { type: String } })
+  await mongoose.connect(databaseURL)
+  const store = new MongoStore({ mongoose: mongoose })
 
-client = new Client({
-  authStrategy: new LocalAuth(),
-  //puppeteer: { headless: true }
-  puppeteer: {
-    args: ['--no-sandbox']
-  }
-})
-if (!parentNumber) throw new Error('You must provide a parent number')
+  client = new Client({
+    // authStrategy: new LocalAuth(),
+    // //puppeteer: { headless: true }
+    // puppeteer: {
+    //   args: ['--no-sandbox']
+    // }
+    authStrategy: new RemoteAuth({
+      store: store,
+      backupSyncIntervalMs: 300000
+    })
+  })
+  if (!parentNumber) throw new Error('You must provide a parent number ok ?')
+  client.on('qr', (qr) => {
+    console.log('entering in QR')
+    qrcode.generate(qr, { small: true })
+    console.log(`Ver QR http://localhost:${port}/qr`)
+  })
 
-console.log('Lets start!')
+  client.on('ready', () => {
+    console.log('The client is ready')
+    listenMessage()
+  })
 
-client.on('qr', (qr) => {
-  console.log('entering in QR')
-  qrcode.generate(qr, { small: true })
-  console.log(`Ver QR http://localhost:${port}/qr`)
-})
+  client.on('remote_session_saved', () => {
+    console.log('session already saved')
+  })
 
-client.on('ready', () => {
-  console.log('The client is ready')
-  listenMessage()
-})
+  client.on('authenticated', () => {
+    console.log('AUTHENTICATED')
+  })
 
-client.on('authenticated', () => {
-  console.log('AUTHENTICATED')
-})
+  client.on('message_create', (msg) => {
+    const { body, id } = msg
+    const { fromMe } = id
+    if (fromMe) console.log('Outcomming message ', body)
+  })
 
-client.on('message_create', (msg) => {
-  const { body, id } = msg
-  const { fromMe } = id
-  if (fromMe) console.log('Outcomming message ', body)
-})
-
-client.initialize()
+  client.initialize()
+}
+dataBaseConnection()
